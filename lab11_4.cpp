71afdd974363fb6cf4801ef323a90e187d804bce
DifficultIV <kitpisan@gmail.com>
2021-01-15 16:01:48 +0700

    done

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename A>
+void mySwap(A&,A&);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,9 @@ int main(){
 	return 0;
 }
 
+template <typename B>
+void mySwap(B &x,B &y){
+	B hold = x;
+	x = y;
+	y = hold;
+}
